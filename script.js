0cd1e2ede72e7a1126916a60375da461c45eb3f7
Pepto <monsieurfranci@gmail.com>
2023-09-11 15:57:44 +0200

    added query selectors and variables

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const bodySelect = document.querySelector('body')
+const secretN = document.querySelector('.secret')
+const userInput = document.querySelector('.userInput')
+const btnCheck = document.querySelector('.checkBtn')
+const btnReplay = document.querySelector('.replay')
+const position = document.querySelector('.position')
+const scoreSelect = document.querySelector('.score')
+const highScoreSelect = document.querySelector('.highScore')
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
